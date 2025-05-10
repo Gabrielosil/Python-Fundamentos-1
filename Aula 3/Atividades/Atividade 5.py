951e9023409c2f4bced8d64e9c85a75aed123a95
Gabrielosil <gabriel.osilva59@senacsp.edu.br>
2025-05-10 11:45:22 -0300

    Feat: Correção

--- Aula 3/Atividades/Atividade 5.py
+++ Aula 3/Atividades/Atividade 5.py
@@ -1,11 +1,10 @@
 '''Crie um programa que calcule e mostre a soma de todos os números ímpares
 entre 1 e 100.'''
 
-soma_impares = 0
-while True:
-   numero = int(input("Digite um número (0 para parar): "))
-   if numero == 0:
-       break
-   if numero % 2 != 0:
-       soma_impares += numero
-print(f"A soma dos números ímpares digitados é: {soma_impares}")
+soma = 0
+
+for i in range(1, 101):
+    if i % 2 != 0:
+        soma += i
+
+print("A soma dos ímpares de 1 a 100 é:", soma)
